32f7e86c8ff5862fa4f7922bc4b4336a9729169d
Younes Jaaidi <yjaaidi@gmail.com>
2020-12-01 14:40:19 +0100

    feat(cypress-harness): ✅ add chainable typed methods to `getHarness` result

--- packages/cypress-harness/src/lib/get-harness.ts
+++ packages/cypress-harness/src/lib/get-harness.ts
@@ -1,6 +1,6 @@
 import { ComponentHarness, HarnessQuery } from '@angular/cdk/testing';
 import { CypressHarnessEnvironment } from './cypress-harness-environment';
-import { getTestBedRoot } from './internals';
+import { addHarnessMethodsToChainer, getTestBedRoot } from './internals';
 
 export function getHarness<T extends ComponentHarness>(query: HarnessQuery<T>) {
   /* Create a local variable so `pipe` can log name. */
@@ -9,5 +9,10 @@ export function getHarness<T extends ComponentHarness>(query: HarnessQuery<T>) {
       query
     );
 
-  return getTestBedRoot().pipe(getHarness);
+  const harnessType = 'harnessType' in query ? query.harnessType : query;
+
+  return addHarnessMethodsToChainer(
+    getTestBedRoot().pipe(getHarness),
+    harnessType
+  );
 }
