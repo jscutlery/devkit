1a5c4fea31a63f983579e3d21d245759b83556f5
Younes Jaaidi <yjaaidi@gmail.com>
2020-12-01 11:42:49 +0100

    chore(cypress-harness): 🛠 remove useless test

--- packages/cypress-harness/src/lib/cypress-harness.spec.ts
+++
@@ -1,7 +0,0 @@
-import { cypressHarness } from './cypress-harness';
-
-describe('cypressHarness', () => {
-  it('should work', () => {
-    expect(cypressHarness()).toEqual('cypress-harness');
-  });
-});
