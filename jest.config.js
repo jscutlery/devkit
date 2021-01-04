e29e3e53abff5ef7837f8866552a48cd83289b53
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-04 17:03:47 +0100

    wip(cypress-harness): 🚧 add test for zone issue
    Cf. #2
    
    nx g @nrwl/angular:application cypress-harness-sandbox

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   projects: [
     '<rootDir>/packages/cypress-harness',
     '<rootDir>/packages/cypress-mount',
+    '<rootDir>/packages/cypress-harness-sandbox',
   ],
 };
---
+++ packages/cypress-harness-sandbox/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-utils-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'cypress-harness-sandbox';
+}
