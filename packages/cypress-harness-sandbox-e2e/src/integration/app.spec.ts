1917819bbddbbd6883eaf783e4d8088d5099c3f4
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-04 17:10:08 +0100

    wip(cypress-harness): 🚧 add test for zone issue
    Cf. #2

--- packages/cypress-harness-sandbox-e2e/src/integration/app.spec.ts
+++ packages/cypress-harness-sandbox-e2e/src/integration/app.spec.ts
@@ -1,13 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
 describe('cypress-harness-sandbox', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to cypress-harness-sandbox!');
+    cy.get('h1').contains(`🚀 Let's test!`);
   });
 });
--- packages/cypress-harness-sandbox/src/app/app.component.ts
+++ packages/cypress-harness-sandbox/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'test-utils-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `<h1>🚀 Let's test!</h1>`,
 })
-export class AppComponent {
-  title = 'cypress-harness-sandbox';
-}
+export class AppComponent {}
