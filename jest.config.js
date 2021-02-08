6fea76c4c68a9fb66bd261dbf44ef37347892ef5
Younes Jaaidi <yjaaidi@gmail.com>
2021-02-08 19:13:21 +0100

    chore: 🛠 add cypress-mount-integration

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/packages/cypress-harness',
     '<rootDir>/packages/cypress-mount',
     '<rootDir>/packages/cypress-harness-sandbox',
+    '<rootDir>/packages/delete-this-app',
   ],
 };
---
+++ packages/cypress-mount-integration/src/integration/app.spec.ts
@@ -0,0 +1,6 @@
+describe('delete-this-app', () => {
+  beforeEach(() => cy.visit('/'));
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  it('should display welcome message', () => {});
+});
