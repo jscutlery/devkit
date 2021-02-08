ef62d1fa37f1589d7f0234030fc9d83b212821cd
Younes Jaaidi <yjaaidi@gmail.com>
2021-02-08 18:58:11 +0100

    chore: 🧹 rename cypress-harness-sandbox to cypress-sandbox

--- packages/cypress-sandbox/jest.config.js
+++ packages/cypress-sandbox/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'cypress-harness-sandbox',
+  displayName: 'cypress-sandbox',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -14,7 +14,7 @@ module.exports = {
       },
     },
   },
-  coverageDirectory: '../../coverage/packages/cypress-harness-sandbox',
+  coverageDirectory: '../../coverage/packages/cypress-sandbox',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
