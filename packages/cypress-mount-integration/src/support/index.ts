cd2aadc645b3b4134752b6f32eb5db317de4780d
Younes Jaaidi <yjaaidi@gmail.com>
2021-02-08 19:41:58 +0100

    fix: 🐞 add dependency injection support

--- packages/cypress-mount-integration/src/support/index.ts
+++ packages/cypress-mount-integration/src/support/index.ts
@@ -1,18 +1,1 @@
 import '@jscutlery/cypress-mount/support';
-
-/**
- * @hack fixes dependency injection in JIT
- */
-import 'reflect-metadata';
-
-
-/**
- * @hack we have to import `zone.js/dist/zone-testing`.
- * This is due to implicit call to `resetFakeAsyncZone()`
- * in `@angular/core/testing`.
- * Cf. https://github.com/jscutlery/test-utils/issues/2
- */
-import 'zone.js/dist/zone';
-/* @hack fixes "Mocha has already been patched with Zone" error. */
-(globalThis as unknown)['Mocha']['__zone_patch__'] = false;
-import 'zone.js/dist/zone-testing';
