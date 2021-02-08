24cf0797e25bc609aab10f8df48042da1a7dffda
Younes Jaaidi <yjaaidi@gmail.com>
2021-02-08 19:21:22 +0100

    chore: 🧹 remove useless files

--- packages/cypress-mount-integration/src/components/dependency-injection.spec.ts
+++ packages/cypress-mount-integration/src/components/dependency-injection.spec.ts
@@ -1,6 +1,4 @@
 describe('delete-this-app', () => {
-  beforeEach(() => cy.visit('/'));
-
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   it('should display welcome message', () => {});
 });
