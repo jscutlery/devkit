1411e130c786c7491fffc8c53b872bcc75791035
Younes Jaaidi <yjaaidi@gmail.com>
2021-02-09 10:21:12 +0100

    chore: 🛠 fix jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,5 @@ module.exports = {
   projects: [
     '<rootDir>/packages/cypress-harness',
     '<rootDir>/packages/cypress-mount',
-    '<rootDir>/packages/cypress-harness-sandbox',
-    '<rootDir>/packages/delete-this-app',
   ],
 };
