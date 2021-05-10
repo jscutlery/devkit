b172d6bc60433321255d11a85fb4792ba3892e7b
Younes Jaaidi <yjaaidi@gmail.com>
2021-05-10 19:37:27 +0200

    wip: 🚧 add first spec for @jscutlery/cypress-angular-dev-server
    #42

--- packages/cypress-angular-dev-server/src/lib/start-angular-dev-server.spec.ts
+++ packages/cypress-angular-dev-server/src/lib/start-angular-dev-server.spec.ts
@@ -1,51 +1,74 @@
 import { startAngularDevServer } from './start-angular-dev-server';
 
+import { startDevServer } from '@cypress/webpack-dev-server';
+import { AngularCompilerPlugin } from '@ngtools/webpack';
+
+/* Import jest functions manually as they conflict with cypress
+ * because @cypress/webpack-dev-server references cypress types */
+import { describe, expect, it } from '@jest/globals';
+
+jest.mock('@cypress/webpack-dev-server');
+jest.mock('@ngtools/webpack');
+
+const mockStartDevServer = startDevServer as jest.MockedFunction<
+  typeof startDevServer
+>;
+const mockAngularCompilerPlugin = AngularCompilerPlugin as jest.MockedClass<
+  typeof AngularCompilerPlugin
+>;
+
 describe(startAngularDevServer.name, () => {
   describe('with default config', () => {
-    // beforeEach(() => {
-    // @todo mock startDevServer return value with {port: 4300}
-    // @todo mock AngularWebpackPlugin
-    // })
+    beforeEach(() => {
+      mockStartDevServer.mockResolvedValue({
+        port: 4200,
+        close: jest.fn(),
+      });
+    });
 
-    // afterEach(() => {
-    //   reset mocks
-    // })
+    afterEach(() => {
+      mockStartDevServer.mockReset();
+      mockAngularCompilerPlugin.mockReset();
+    });
 
     xit(`should call startDevServer with the right webpack options`, async () => {
-      // @todo type options as DevServerOptions & config as PluginConfigOptions
-      // await startAngularDevServer({ config, options });
-      // @todo check startDevServer was called with
-      // expect(resolvedConfig).toEqual({
-      //   expect.objectContaining{
-      //     port: 4300
-      //   }
-      // })
-      // expect(startDevServer).toBeCalledTimes(1);
-      // expect(startDevServer).toBeCalledWith({
-      //   webpackConfig: {
-      //     devtool: false,
-      //     plugins: [expect.any(AngularCompilerPlugin)],
-      //     resolve: {
-      //       extensions: ['.js', '.ts'],
-      //     },
-      //     module: {
-      //       rules: [
-      //         {
-      //           test: /\.ts$/,
-      //           loader: '@ngtools/webpack',
-      //         },
-      //         {
-      //           test: /\.css$/,
-      //           loader: 'raw-loader',
-      //         },
-      //         {
-      //           test: /\.scss$/,
-      //           use: ['raw-loader', 'sass-loader'],
-      //         },
-      //       ],
-      //     },
-      //   },
-      // });
+      const resolvedConfig = await startAngularDevServer({
+        config: {} as Cypress.PluginConfigOptions,
+        options: {} as Cypress.DevServerOptions,
+      });
+
+      expect(resolvedConfig).toEqual(
+        expect.objectContaining({
+          port: 4300,
+        })
+      );
+
+      expect(startDevServer).toBeCalledTimes(1);
+      expect(startDevServer).toBeCalledWith({
+        webpackConfig: {
+          devtool: false,
+          plugins: [expect.any(AngularCompilerPlugin)],
+          resolve: {
+            extensions: ['.js', '.ts'],
+          },
+          module: {
+            rules: [
+              {
+                test: /\.ts$/,
+                loader: '@ngtools/webpack',
+              },
+              {
+                test: /\.css$/,
+                loader: 'raw-loader',
+              },
+              {
+                test: /\.scss$/,
+                use: ['raw-loader', 'sass-loader'],
+              },
+            ],
+          },
+        },
+      });
     });
 
     xit('should create angular compiler with the right options', async () => {
--- packages/cypress-angular-dev-server/src/lib/start-angular-dev-server.ts
+++ packages/cypress-angular-dev-server/src/lib/start-angular-dev-server.ts
@@ -1,6 +1,14 @@
+/// <reference types="cypress"/>
+
 /**
-* @deprecated 🚧 Work in progress.
-*/
-export function startAngularDevServer() {
+ * @deprecated 🚧 Work in progress.
+ */
+export function startAngularDevServer({
+  config,
+  options,
+}): {
+  config: Cypress.PluginConfigOptions;
+  options: Cypress.DevServerOptions;
+} {
   throw new Error('🚧 Work in progress!');
 }
