f1e9dd8aa73f0c21400ea23a2628f7f168e2db96
Younes Jaaidi <yjaaidi@gmail.com>
2021-05-15 21:53:46 +0200

    wip(cypress-angular): 🚧 add specs
    #48

--- packages/cypress-angular/src/generators/setup-ct/generator.spec.ts
+++ packages/cypress-angular/src/generators/setup-ct/generator.spec.ts
@@ -1,18 +1,37 @@
-import { Tree } from '@nrwl/devkit';
+import { addProjectConfiguration, Tree } from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import generator from './generator';
 import { SetupCtGeneratorSchema } from './schema';
 
 describe('setup-ct generator', () => {
-  let appTree: Tree;
-  const options: SetupCtGeneratorSchema = { project: 'my-lib' };
+  let tree: Tree;
 
   beforeEach(async () => {
-    appTree = createTreeWithEmptyWorkspace();
-    await generator(appTree, options);
+    tree = createTreeWithEmptyWorkspace();
+    addProjectConfiguration(tree, 'my-lib', {
+      root: 'libs/my-lib',
+      targets: {},
+    });
+    await generator(tree, {
+      project: 'my-lib',
+    } as SetupCtGeneratorSchema);
   });
 
-  it.todo('should add libs/my-lib/cypress/plugins/index.ts');
+  xit('should add libs/my-lib/cypress/plugins/index.ts', () => {
+    expect(
+      tree.read('libs/my-lib/cypress/plugins/index.ts').toString()
+    ).toContain(`startAngularDevServer({ config, options })`);
+  });
 
-  it.todo('should add libs/my-lib/cypress.json');
+  xit('should add libs/my-lib/cypress.json', () => {
+    expect(tree.read('libs/my-lib/cypress.json').toJSON()).toEqual(
+      expect.objectContaining({
+        pluginsFile: './cypress/plugins/index.ts',
+        component: {
+          testFiles: '**/*.cy-spec.{js,ts,jsx,tsx}',
+          componentFolder: './src',
+        },
+      })
+    );
+  });
 });
