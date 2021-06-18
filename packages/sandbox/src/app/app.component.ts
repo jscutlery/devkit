1ef208435698539f4726e5c9147406fcee21caed
Edouard Bozon <bozonedouard@gmail.com>
2021-06-18 08:50:53 +0200

    fix(cypress-harness): 🐞 correct return type of `getAllHarnesses`

--- packages/sandbox/src/app/app.component.ts
+++ packages/sandbox/src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { FormModule } from './form.component';
 import { TitleModule } from './title.component';
 
 @Component({
   selector: 'jc-sandbox-app',
-  template: `<jc-sandbox-title></jc-sandbox-title>`,
+  template: `
+    <jc-sandbox-title></jc-sandbox-title>
+    <jc-sandbox-form></jc-sandbox-form>
+  `,
 })
 export class AppComponent {}
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TitleModule],
+  imports: [BrowserModule, TitleModule, FormModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
