e604cdd44a66718552e171647c9b37a60ac487a1
Younes Jaaidi <yjaaidi@gmail.com>
2021-10-19 17:13:17 +0200

    wip(operators): add `finalized` to state

--- packages/operators/src/lib/report.ts
+++ packages/operators/src/lib/report.ts
@@ -1,11 +1,11 @@
-import { coalesceWith } from '@rx-angular/cdk/coalescing';
-import { concat, defer, Observable, of, OperatorFunction } from 'rxjs';
-import { filter, map, materialize } from 'rxjs/operators';
+import { Observable, OperatorFunction } from 'rxjs';
+import { map, materialize, scan, startWith } from 'rxjs/operators';
 
 export interface ReportState<T> {
   value: undefined | T;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   error: undefined | any;
+  finalized: boolean;
   pending: boolean;
 }
 
@@ -17,32 +17,39 @@ export function report<T, R = ReportState<T>>(
   projector?: (data: ReportState<T>) => R
 ): OperatorFunction<T, R | ReportState<T>> {
   return (o$: Observable<T>): Observable<R | ReportState<T>> => {
-    const hasProjector = typeof projector === 'function';
-    const pending$ = hasProjector
-      ? of({ value: undefined, error: undefined, pending: true }).pipe(
-          map(projector)
-        )
-      : of({ value: undefined, error: undefined, pending: true });
+    const initialState = {
+      value: undefined,
+      error: undefined,
+      finalized: false,
+      pending: true,
+    };
 
-    return concat(
-      pending$,
-      o$.pipe(
-        materialize(),
-        filter((notification) => notification.kind !== 'C'),
-        map((n) =>
-          hasProjector
-            ? projector({
-                value: n.value,
-                error: n.error,
-                pending: false,
-              })
-            : {
-                value: n.value,
-                error: n.error,
-                pending: false,
-              }
-        )
-      )
-    ).pipe(coalesceWith(defer(() => Promise.resolve())));
+    const result$ = o$.pipe(
+      materialize(),
+      scan((state = initialState, notification) => {
+        /* On complete, merge `finalized: true & pending: false`
+         * with the current state. */
+        if (notification.kind === 'C') {
+          return {
+            ...state,
+            finalized: true,
+            pending: false,
+          };
+        }
+
+        /* Mark as finalized on error as complete is only triggered on success. */
+        const finalized = notification.kind === 'E';
+
+        return {
+          value: notification.value,
+          error: notification.error,
+          finalized,
+          pending: false,
+        };
+      }, undefined),
+      startWith(initialState)
+    );
+
+    return projector != null ? result$.pipe(map(projector)) : result$;
   };
 }
