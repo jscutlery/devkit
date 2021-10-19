260e4e147a13bd53dcfb6e63066a13aab41329a6
Younes Jaaidi <yjaaidi@gmail.com>
2021-10-19 12:37:03 +0200

    wip(operators): add `report` operator empty specs

--- packages/operators/src/lib/report.spec.ts
+++ packages/operators/src/lib/report.spec.ts
@@ -1,4 +1,19 @@
 import { report } from './report';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-xdescribe(report.name, () => {});
+describe(report.name, () => {
+  describe('successful source', () => {
+    it.todo('should emit result with value');
+  });
+
+  describe('failed source', () => {
+    it.todo('should emit result with error');
+  });
+
+  describe('pending source', () => {
+    it.todo('should emit result with pending=true and without value nor error');
+  });
+
+  describe('failed source after emitting value', () => {
+    it.todo('should emit result with error and without value');
+  });
+});
