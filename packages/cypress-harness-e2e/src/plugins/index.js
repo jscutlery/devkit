fbbbeb657da6e71082419992de3beeed591a3063
Younes Jaaidi <yjaaidi@gmail.com>
2021-12-06 12:26:24 +0100

    chore: 🛠 fix test

--- packages/cypress-harness-e2e/src/plugins/index.js
+++ packages/cypress-harness-e2e/src/plugins/index.js
@@ -14,7 +14,13 @@
 const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor');
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  // Preprocess Typescript file using Nx helper
+  /* @hack using deprecated preprocessTypescript
+   * as there is something wrong with the default Cypress config
+   * that produces the following error when using things like @jscutlery/harness
+   * that imports @angular/cdk/testing:
+   *     Error: Webpack Compilation Error
+   *     /jscutlery/devkit/node_modules/@angular/cdk/fesm2015/testing/testbed.mjs
+   *     Module not found: Error: Can't resolve '@angular/cdk/keycodes' 
+   *     in '/jscutlery/devkit/node_modules/@angular/cdk/fesm2015/testing' */
+  on('file:preprocessor', preprocessTypescript(config));
 };
