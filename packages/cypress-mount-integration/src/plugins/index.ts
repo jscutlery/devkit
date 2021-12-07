1338b19dface3ec0527a17afc4c3fbca82983010
Younes Jaaidi <yjaaidi@gmail.com>
2021-12-08 00:08:46 +0100

    chore(cypress-mount): 🛠 fix tests

--- packages/cypress-mount-integration/src/plugins/index.ts
+++ packages/cypress-mount-integration/src/plugins/index.ts
@@ -1,16 +1,25 @@
 import { startAngularDevServer } from '@jscutlery/cypress-angular-dev-server';
 
-module.exports = (on, config) => {
+module.exports = (
+  on: Cypress.PluginEvents,
+  config: Cypress.RuntimeConfigOptions
+) => {
   on('dev-server:start', (options) =>
     startAngularDevServer({
       config,
       options,
+      /* @hack this fixes the following error:
+       * "Module not found: Error: Can't resolve 'path' in '.../node_modules/@storybook/store/dist/esm'"
+       * as storybook requires 'path' even though it's not used and webpack 5 removed the polyfill. */
       webpackConfig: {
-        node: {
-          global: true,
+        resolve: {
+          fallback: {
+            path: require.resolve('path-browserify'),
+          },
         },
       },
     })
   );
+
   return config;
 };
