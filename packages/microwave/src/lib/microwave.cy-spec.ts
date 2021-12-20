8e05369b1cc55f6aa4bea166a076a25ff475a337
Younes Jaaidi <yjaaidi@gmail.com>
2021-12-20 20:55:04 +0100

    test(microwave): ✅ add cypress component test

--- packages/microwave/src/lib/microwave.cy-spec.ts
+++ packages/microwave/src/lib/microwave.cy-spec.ts
@@ -1,5 +1,67 @@
-import { Microwave } from './microwave';
+import { Component, Input, Output } from '@angular/core';
+import { mount } from '@jscutlery/cypress-angular/mount';
+import { delay, map } from 'rxjs';
+import { Microwave, watch } from './microwave';
 
-describe(Microwave.name, () => {
-  it('🚧 should trigger change detection');
+@Microwave()
+@Component({
+  selector: 'jscutlery-meal',
+  template: `<div data-role="meal-name">{{ capitalizedMeal }}</div>
+    <button (click)="evaluation = 'meh'">😋</button>
+    <button (click)="evaluation = 'delicious'">😒</button>`,
+})
+class GreetingsComponent {
+  @Input() meal?: string;
+  @Output() evaluation$ = watch(this, 'evaluation');
+
+  capitalizedMeal: string;
+
+  evaluation?: string;
+
+  constructor() {
+    watch(this, 'meal')
+      .pipe(
+        delay(10),
+        map((meal) => meal?.toUpperCase())
+      )
+      .subscribe((capitalizedMeal) => (this.capitalizedMeal = capitalizedMeal));
+  }
+}
+
+describe('Microwave', () => {
+  xit('🚧 should trigger change detection on property change', () => {
+    const { getMealEl, setInput } = setUp();
+
+    setInput('meal', 'Burger');
+
+    getMealEl().contains('BURGER');
+
+    setInput('meal', 'Babaganoush');
+
+    getMealEl().contains('BABAGANOUSH');
+  });
+
+  xit('🚧 should unsubscribe on destroy');
+
+  function setUp() {
+    mount(GreetingsComponent);
+
+    function getComponent() {
+      return cy
+        .get('jscutlery-meal')
+        .then((el) => globalThis.ng.getComponent(el.get(0)));
+    }
+
+    return {
+      setInput<K extends keyof GreetingsComponent = keyof GreetingsComponent>(
+        property: keyof GreetingsComponent,
+        value: GreetingsComponent[K]
+      ) {
+        return getComponent().then((cmp) => (cmp[property] = value));
+      },
+      getMealEl() {
+        return cy.get('[data-role=meal-name]');
+      },
+    };
+  }
 });
