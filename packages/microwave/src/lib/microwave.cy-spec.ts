e98e7f5b903cf961c9d3469e3efb31665969867e
Younes Jaaidi <yjaaidi@gmail.com>
2021-12-21 12:22:06 +0100

    test(microwave): ✅ add tests

--- packages/microwave/src/lib/microwave.cy-spec.ts
+++ packages/microwave/src/lib/microwave.cy-spec.ts
@@ -1,14 +1,24 @@
-import { Component, Input, Output } from '@angular/core';
-import { mount } from '@jscutlery/cypress-angular/mount';
-import { delay, map } from 'rxjs';
+import { Component, Injectable, Input, Output } from '@angular/core';
+import { destroy, mount } from '@jscutlery/cypress-angular/mount';
+import { delay, finalize, map } from 'rxjs';
 import { Microwave, watch } from './microwave';
+
+/**
+ * A service to detect when watch is unsubscribed from.
+ */
+@Injectable()
+class Snitch {
+  finalized = false;
+}
 
 @Microwave()
 @Component({
   selector: 'jscutlery-meal',
   template: `<div data-role="meal-name">{{ capitalizedMeal }}</div>
-    <button (click)="evaluation = 'meh'">😋</button>
-    <button (click)="evaluation = 'delicious'">😒</button>`,
+    <button data-role="meh" (click)="evaluation = 'meh'">😋</button>
+    <button data-role="delicious" (click)="evaluation = 'delicious'">
+      😒
+    </button>`,
 })
 class GreetingsComponent {
   @Input() meal?: string = undefined;
@@ -18,15 +28,14 @@ class GreetingsComponent {
 
   evaluation?: string = undefined;
 
-  constructor() {
+  constructor(snitch: Snitch) {
     watch(this, 'meal')
       .pipe(
+        finalize(() => (snitch.finalized = true)),
         delay(10),
         map((meal) => meal?.toUpperCase())
       )
-      .subscribe((capitalizedMeal) => {
-        return (this.capitalizedMeal = capitalizedMeal);
-      });
+      .subscribe((capitalizedMeal) => (this.capitalizedMeal = capitalizedMeal));
   }
 }
 
@@ -43,12 +52,45 @@ describe('Microwave', () => {
     getMealEl().contains('BABAGANOUSH');
   });
 
-  xit('🚧 should trigger evaluationChange output');
+  it('should emit evaluationChange output on initialization', () => {
+    const { evaluationChangeStub } = setUp();
 
-  xit('🚧 should unsubscribe on destroy');
+    cy.wrap(evaluationChangeStub).should('be.calledOnceWith', undefined);
+  });
+
+  it('should emit evaluationChange output on click', () => {
+    const { clickEvaluation, evaluationChangeStub } = setUp();
+
+    cy.wrap(evaluationChangeStub).invoke('reset');
+
+    clickEvaluation('delicious');
+
+    cy.wrap(evaluationChangeStub).should('be.calledOnceWith', 'delicious');
+  });
+
+  it('should unsubscribe on destroy', () => {
+    const { snitch } = setUp();
+
+    destroy();
+
+    cy.wrap(snitch).its('finalized').should('equal', true);
+  });
 
   function setUp() {
-    mount(GreetingsComponent);
+    const evaluationChangeStub = cy.stub();
+    const snitch = new Snitch();
+
+    mount(GreetingsComponent, {
+      providers: [
+        {
+          provide: Snitch,
+          useValue: snitch,
+        },
+      ],
+      outputs: {
+        evaluationChange: evaluationChangeStub,
+      },
+    });
 
     function getComponent() {
       return cy
@@ -57,6 +99,11 @@ describe('Microwave', () => {
     }
 
     return {
+      evaluationChangeStub,
+      snitch,
+      clickEvaluation(evaluation: 'delicious' | 'meh') {
+        return cy.get(`[data-role=${evaluation}]`).click();
+      },
       setInput<K extends keyof GreetingsComponent = keyof GreetingsComponent>(
         property: keyof GreetingsComponent,
         value: GreetingsComponent[K]
