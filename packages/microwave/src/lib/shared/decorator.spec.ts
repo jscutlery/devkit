851a5c01a3a7cb9ab6a075b471b5dfb894c307a6
Younes Jaaidi <yjaaidi@gmail.com>
2021-12-22 09:52:38 +0100

    test(microwave): 🚧 add decorateComponent empty specs

---
+++ packages/microwave/src/lib/shared/decorator.spec.ts
@@ -0,0 +1,51 @@
+import { decorateComponent, DecoratorHooks } from './decorator';
+
+describe(decorateComponent.name, () => {
+  it.todo('🚧 should trigger real ngOnInit');
+
+  it.todo('🚧 should trigger real ngOnDestroy');
+
+  it.todo('🚧 should trigger onCreate');
+
+  it.todo('🚧 should trigger onInit');
+
+  it.todo('🚧 should trigger onDestroy');
+
+  it.todo('🚧 should trigger onPropertyDeclare');
+
+  it.todo('🚧 should trigger onPropertyGet');
+
+  it.todo('🚧 should trigger onPropertySet');
+
+  function setUp() {
+    const ngOnInit = jest.fn();
+    const ngOnDestroy = jest.fn();
+
+    class MyComponent {
+      something?: number = undefined;
+
+      ngOnInit() {
+        ngOnInit();
+      }
+
+      ngOnDestroy() {
+        ngOnDestroy();
+      }
+    }
+
+    const mockHooks: jest.Mocked<DecoratorHooks<MyComponent>> = {
+      onCreate: jest.fn(),
+      onDestroy: jest.fn(),
+      onPropertyDeclare: jest.fn(),
+      onPropertyGet: jest.fn(),
+      onPropertySet: jest.fn(),
+    };
+
+    return {
+      MyComponent,
+      mockHooks,
+      ngOnInit,
+      ngOnDestroy,
+    };
+  }
+});
