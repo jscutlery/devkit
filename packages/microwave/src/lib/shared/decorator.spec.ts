92dd1923574108c8c28c33b2a7d74af64bfa63da
Younes Jaaidi <yjaaidi@gmail.com>
2021-12-22 10:21:29 +0100

    test(microwave): ✅ test decorateComponent

--- packages/microwave/src/lib/shared/decorator.spec.ts
+++ packages/microwave/src/lib/shared/decorator.spec.ts
@@ -1,30 +1,74 @@
+import { TestBed } from '@angular/core/testing';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { createTestingBowl } from '../../../testing/testing-bowl';
 import { decorateComponent, DecoratorHooks } from './decorator';
 
 describe(decorateComponent.name, () => {
   const bowl = createTestingBowl(setUp);
 
-  it.todo('🚧 should trigger real ngOnInit');
+  it('should trigger real ngOnInit', () => {
+    const { cmp, ngOnInit } = bowl;
+    cmp.ngOnInit();
+    expect(ngOnInit).toBeCalledTimes(1);
+  });
 
-  it.todo('🚧 should trigger real ngOnDestroy');
+  it('should trigger real ngOnDestroy', () => {
+    const { cmp, ngOnDestroy } = bowl;
+    cmp.ngOnDestroy();
+    expect(ngOnDestroy).toBeCalledTimes(1);
+  });
 
-  it.todo('🚧 should trigger onCreate');
+  it('should trigger onCreate', () => {
+    const { mockHooks } = bowl;
+    expect(mockHooks.onCreate).toBeCalledTimes(1);
+  });
 
   it.todo('🚧 should trigger onInit');
 
-  it.todo('🚧 should trigger onDestroy');
+  it('should trigger onDestroy', () => {
+    const { cmp, mockHooks } = bowl;
+    cmp.ngOnDestroy();
+    expect(mockHooks.onDestroy).toBeCalledTimes(1);
+  });
 
-  it.todo('🚧 should trigger onPropertyDeclare');
+  it('should trigger onPropertyDeclare', () => {
+    const { cmp, mockHooks } = bowl;
+    expect(mockHooks.onPropertyDeclare).toBeCalledTimes(1);
+    expect(mockHooks.onPropertyDeclare).toBeCalledWith(
+      cmp,
+      'something',
+      undefined
+    );
+  });
 
-  it.todo('🚧 should trigger onPropertyGet');
+  it('should trigger onPropertyGet', () => {
+    const { cmp, mockHooks } = bowl;
 
-  it.todo('🚧 should trigger onPropertySet');
+    mockHooks.onPropertyGet.mockReturnValue(42);
+
+    expect(cmp.something).toEqual(42);
+
+    expect(mockHooks.onPropertyGet).toBeCalledTimes(1);
+    expect(mockHooks.onPropertyGet).toBeCalledWith(cmp, 'something');
+  });
+
+  it('should trigger onPropertySet', () => {
+    const { cmp, mockHooks } = bowl;
+
+    cmp.something = 42;
+
+    expect(mockHooks.onPropertySet).toBeCalledTimes(1);
+    expect(mockHooks.onPropertySet).toBeCalledWith(cmp, 'something', 42);
+  });
 
   function setUp() {
     const ngOnInit = jest.fn();
     const ngOnDestroy = jest.fn();
 
-    class MyComponent {
+    @Component({
+      template: '',
+    })
+    class MyComponent implements OnInit, OnDestroy {
       something?: number = undefined;
 
       ngOnInit() {
@@ -44,8 +88,16 @@ describe(decorateComponent.name, () => {
       onPropertySet: jest.fn(),
     };
 
+    decorateComponent(MyComponent, mockHooks);
+
+    TestBed.configureTestingModule({
+      providers: [MyComponent, { provide: ChangeDetectorRef, useValue: {} }],
+    });
+
+    const cmp = TestBed.inject(MyComponent);
+
     return {
-      MyComponent,
+      cmp,
       mockHooks,
       ngOnInit,
       ngOnDestroy,
