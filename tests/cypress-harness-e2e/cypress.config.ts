376648d7bcd8537fcc5a632ed3bd50de103635b5
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-01 09:26:58 +0100

    test(cypress-harness): ✅ fix tests

--- tests/cypress-harness-e2e/cypress.config.ts
+++ tests/cypress-harness-e2e/cypress.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+import webpackPreprocessor from '@cypress/webpack-batteries-included-preprocessor';
 
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname),
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    setupNodeEvents(on) {
+      on('file:preprocessor', webpackPreprocessor({
+        typescript: require.resolve('typescript')
+      }));
+    }
+  }
 });
