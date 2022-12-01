d2eb816acd4f4c43cbab625a49c232b1cfb29bc1
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-01 11:00:18 +0100

    test: ✅ rename "component-test" target to "test"
    and rename build target which is only used
    to pass build options to the component test

--- tests/cypress-harness-wide/cypress.config.ts
+++ tests/cypress-harness-wide/cypress.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'cypress';
 import { nxComponentTestingPreset } from '@nrwl/angular/plugins/component-testing';
 
 export default defineConfig({
-  component: nxComponentTestingPreset(__filename),
+  component: nxComponentTestingPreset(__filename, {
+    ctTargetName: 'test'
+  }),
   viewportWidth: 960,
   viewportHeight: 540,
 });
