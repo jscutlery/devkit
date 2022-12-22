f780c58c5fadc420ad450fdeaa5154ff20550aa7
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-22 11:23:51 +0100

    test(eslint-plugin): ✅ split tests

--- packages/eslint-plugin/src/lib/rules/max-public-exports.spec.ts
+++ packages/eslint-plugin/src/lib/rules/max-public-exports.spec.ts
@@ -1,36 +1,56 @@
-import { TSESLint } from '@typescript-eslint/utils';
-import rule, { RULE_NAME } from './max-public-exports';
+import { describe, expect, it } from '@jest/globals';
 import * as parser from '@typescript-eslint/parser';
+import { TSESLint } from '@typescript-eslint/utils';
+import rule, { Options, RULE_NAME } from './max-public-exports';
 
 describe(RULE_NAME, () => {
-  it('should pass', () => {
-    const result = runRule(`export { A } from './a';`, 'foo.ts', {
+  it('should allow few public exports when max is set', () => {
+    const result = runRule(`export { A, B, C } from './a';`, 'foo.ts', {
       max: 3,
-      noExportAll: true,
     });
-    expect(result.length).toBe(0);
+
+    expect(result).toEqual([]);
   });
 
-  it('should fail', () => {
-    const result = runRule(
-      `
-    export { A, B, C, D, E, F, G, H, I, J, K, L, M } from './a';
-    export * from './a';`,
-      'foo.ts',
-      {
-        max: 3,
-        noExportAll: true,
-      }
-    );
-    expect(result.length).toBe(2);
-    expect(result[0].messageId).toBe('max-public-exports');
-    expect(result[1].messageId).toBe('no-use-of-export-all');
+  it('should reject too many public exports when max is set', () => {
+    const result = runRule(`export { A, B, C, D } from './a';`, 'foo.ts', {
+      max: 3,
+    });
+
+    expect(result).toEqual([
+      expect.objectContaining({
+        messageId: 'max-public-exports',
+      }),
+    ]);
+  });
+
+  it('should allow explicit exports when noExportAll is set to true', () => {
+    const result = runRule(`export { A } from './a';`, 'foo.ts', {
+      noExportAll: true,
+    });
+    expect(result).toEqual([]);
+  });
+
+  it('should reject barrel exports when noExportAll is set to true', () => {
+    const result = runRule(`export * from './a';`, 'foo.ts', {
+      noExportAll: true,
+    });
+
+    expect(result).toEqual([
+      expect.objectContaining({
+        messageId: 'no-use-of-export-all',
+      }),
+    ]);
   });
 });
 
 const linter = new TSESLint.Linter();
 
-function runRule(content: string, contentPath: string, ruleArguments: any) {
+function runRule(
+  content: string,
+  contentPath: string,
+  ruleArguments: Options[0]
+) {
   const config = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
