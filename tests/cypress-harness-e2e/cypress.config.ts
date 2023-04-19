3b1b131ff3b231cde67ce3a7364d0715a8621045
Edouard Bozon <bozonedouard@gmail.com>
2023-04-19 08:24:14 +0200

    build: 🔧 remove cypress flag

--- tests/cypress-harness-e2e/cypress.config.ts
+++ tests/cypress-harness-e2e/cypress.config.ts
@@ -1,17 +1,10 @@
-import { defineConfig } from 'cypress';
-import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 import { getPreprocessorConfig } from '@jscutlery/cypress-harness/preprocessor-config';
+import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     ...nxE2EPreset(__dirname),
     ...getPreprocessorConfig(),
-    /**
-    * TODO(@nrwl/cypress): In Cypress v12,the testIsolation option is turned on by default. 
-    * This can cause tests to start breaking where not indended.
-    * You should consider enabling this once you verify tests do not depend on each other
-    * More Info: https://docs.cypress.io/guides/references/migration-guide#Test-Isolation
-    **/
-    testIsolation: false,
  }
 });
