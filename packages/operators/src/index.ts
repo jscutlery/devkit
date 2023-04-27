96eed664a96e86e9e21e107b89d220a2f0addd9e
Younes Jaaidi <yjaaidi@gmail.com>
2023-04-28 01:04:04 +0200

    fix(operators): 🐞 export missing types + pending const

--- packages/operators/src/index.ts
+++ packages/operators/src/index.ts
@@ -1,1 +1,10 @@
-export { suspensify, Suspense } from './lib/suspensify';
+export { pending, suspensify } from './lib/suspensify';
+export type {
+  SuspenseLax,
+  Suspense,
+  SuspenseEmpty,
+  SuspensePending,
+  SuspenseWithError,
+  SuspenseWithValue,
+  SuspensifyOptions,
+} from './lib/suspensify';
