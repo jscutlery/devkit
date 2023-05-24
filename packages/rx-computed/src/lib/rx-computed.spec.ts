8a69ab4848597f081ce8224c3fae049eedbaac7e
Younes Jaaidi <yjaaidi@gmail.com>
2023-05-25 00:49:02 +0200

    test(rx-computed): ✅ add rxComputed empty specs

---
+++ packages/rx-computed/src/lib/rx-computed.spec.ts
@@ -0,0 +1,15 @@
+import { rxComputed } from './rx-computed';
+
+describe(rxComputed.name, () => {
+
+  it.todo('should return initial value');
+
+  it.todo('should return emitted sync value');
+
+  it.todo('should return emitted async value');
+
+  it.todo('should throw error');
+
+  it.todo('should throw unsubscribe when dependency changes');
+
+});
