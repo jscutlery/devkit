f14b091326ceb6aaf0cbf7ebcb00637aac1477fd
Adrian Baran <adbaran@cisco.com>
2023-12-05 12:41:28 -0600

    fix: import of zone.js and zone.js/testing

--- packages/cypress-harness/src/support.ts
+++ packages/cypress-harness/src/support.ts
@@ -6,10 +6,10 @@ import 'cypress-pipe';
  * in `@angular/core/testing`.
  * Cf. https://github.com/jscutlery/devkit/issues/2
  */
-import 'zone.js/dist/zone';
+import 'zone.js';
 
 /* @hack fixes "Mocha has already been patched with Zone" error. */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (globalThis as any)['Mocha']['__zone_patch__'] = false;
 
-import 'zone.js/dist/zone-testing';
+import 'zone.js/testing';
