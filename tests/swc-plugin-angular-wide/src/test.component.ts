8d1f61fa349ad9e9e93ef50b0f350d96f3afab09
Younes Jaaidi <younes@marmicode.io>
2024-03-22 16:21:39 +0100

    fix(swc-plugin-angular): 🐞 fix crash by downgrading @swc/core

--- tests/swc-plugin-angular-wide/src/test.component.ts
+++ tests/swc-plugin-angular-wide/src/test.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   templateUrl: './test.component.html',
-  styleUrl: './test.component.css',
+  styleUrls: ['./test.component.css'],
 })
 export class TestComponent {}
