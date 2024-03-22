33fc6644bc0965ef8546a9442e78a0520e60ff57
Younes Jaaidi <younes@marmicode.io>
2024-03-22 11:09:15 +0100

    refactor(swc-plugin-angular): 🛠️ add swc-plugin-angular plugin
    Co-authored-by: Edouard Bozon <bozonedouard@gmail.com>

---
+++ packages/swc-plugin-angular/src/lib.rs
@@ -0,0 +1,202 @@
+use swc_core::ecma::{
+    ast::{ArrayLit, CallExpr, Expr, ExprOrSpread, Ident, Lit, Program, PropName, Str},
+    visit::{as_folder, FoldWith, VisitMut, VisitMutWith },
+};
+use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
+use swc_ecma_utils::ExprFactory;
+
+pub struct AngularTransormVisitor {
+    is_in_component_call: bool,
+    is_in_decorator: bool
+}
+
+impl AngularTransormVisitor {
+    pub fn new() -> Self {
+        AngularTransormVisitor { is_in_component_call: false, is_in_decorator: false }
+    }
+}
+impl VisitMut for AngularTransormVisitor {
+
+    fn visit_mut_decorator(&mut self, node: &mut swc_core::ecma::ast::Decorator) {
+        /* Locate `@Component()` decorator. */
+        self.is_in_decorator = true;
+        node.visit_mut_children_with(self);
+        self.is_in_decorator = false;
+    }
+
+    fn visit_mut_call_expr(&mut self, node: &mut swc_core::ecma::ast::CallExpr) {
+        /* Locate `_ts_decorate`. */
+        if let Some(ident) = node.callee.as_expr().and_then(|e| e.as_ident()) {
+            if ident.sym.eq("_ts_decorate") {
+                self.is_in_decorator = true;
+                node.visit_mut_children_with(self);
+                self.is_in_decorator = false;
+                return
+            }
+        }
+
+        /* Locate `Component` function call inside `_ts_decorate`. */
+        if self.is_in_decorator {
+            if let Some(ident) = node.callee.as_expr().and_then(|e| e.as_ident()) {
+                if ident.sym.eq("Component") {
+                    self.is_in_component_call = true;
+                    node.visit_mut_children_with(self);
+                    self.is_in_component_call = false;
+                    return
+                }
+            }
+        }
+
+        node.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_key_value_prop(&mut self, node: &mut swc_core::ecma::ast::KeyValueProp) {
+        if !self.is_in_component_call {
+            return
+        }
+
+        if node.key.as_ident().unwrap().sym.eq("styleUrls") {
+            node.key = PropName::Ident(Ident {
+                sym: "styles".into(),
+                span: Default::default(),
+                optional: false,
+            });
+            node.value = Expr::Array(ArrayLit { span: Default::default(), elems: vec![] }).into();
+        }
+
+        if node.key.as_ident().unwrap().sym.eq("templateUrl") {
+            node.key = PropName::Ident(Ident {
+                sym: "template".into(),
+                span: Default::default(),
+                optional: false,
+            });
+
+            let value = match &*node.value {
+                Expr::Lit(Lit::Str(s)) => s.value.as_ref(),
+                _ => panic!("templateUrl value is not a string")
+            };
+
+            node.value = Expr::Call(CallExpr {
+                span: Default::default(),
+                callee: Expr::Ident(Ident {
+                    sym: "require".into(),
+                    span: Default::default(),
+                    optional: false,
+                }).as_callee(),
+                args: vec![ExprOrSpread {
+                    spread: None,
+                    expr: Lit::Str(Str {
+                        span: Default::default(),
+                        value: value.into(),
+                        raw: None,
+                    }).into(),
+                }],
+                type_args: None,
+            }).into();
+
+        }
+    }
+}
+
+
+#[plugin_transform]
+pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
+    let visitor = AngularTransormVisitor::new();
+    return program.fold_with(&mut as_folder(visitor));
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use swc_core::ecma::{transforms::testing::test_inline, visit::as_folder};
+    use swc_ecma_parser::{Syntax, TsConfig};
+
+    test_inline!(
+        Syntax::Typescript(TsConfig {
+            decorators: true,
+            ..Default::default()
+        }),
+        |_| as_folder(AngularTransormVisitor::new()),
+        replace_template_url,
+        // Input codes
+        r#"
+        @Component({
+            selector: 'app-hello',
+            styleUrls: ['./style.css'],
+            templateUrl: './hello.component.html'
+        })
+        class MyCmp {}"#,
+        // Output codes after transformed with plugin
+        r#"
+        @Component({
+            selector: 'app-hello',
+            styles: [],
+            template: require("./hello.component.html")
+        })
+        class MyCmp {}"#
+    );
+
+    /* SWC transforms decorators before running the plugin. */
+    test_inline!(
+        Syntax::Typescript(TsConfig {
+            decorators: true,
+            ..Default::default()
+        }),
+        |_| as_folder(AngularTransormVisitor::new()),
+        replace_template_url_with_ts_decorate,
+        // Input codes
+        r#"
+        function MyCmp () {}
+        MyCmp = _ts_decorate([
+            Component({
+                selector: 'app-hello',
+                styleUrls: ['./style.css'],
+                templateUrl: './hello.component.html'
+            })
+        ], MyCmp);"#,
+        r#"
+        function MyCmp () {}
+        MyCmp = _ts_decorate([
+            Component({
+                selector: 'app-hello',
+                styles: [],
+                template: require("./hello.component.html")
+            })
+        ], MyCmp);"#
+    );
+
+    test_inline!(
+        Syntax::Typescript(TsConfig {
+            decorators: true,
+            ..Default::default()
+        }),
+        |_| as_folder(AngularTransormVisitor::new()),
+        replace_template_url_in_component_decorator_only,
+        // Input codes
+        r#"
+        const something = {templateUrl: './hello.component.html'};
+        @Component({
+            selector: 'app-hello',
+            styleUrls: ['./style.css'],
+            templateUrl: './hello.component.html'
+        })
+        @MyDecorator({
+            templateUrl: 'something'
+        })
+        class MyCmp {}"#,
+        // Output codes after transformed with plugin
+        r#"
+        const something = {templateUrl: './hello.component.html'};
+        @Component({
+            selector: 'app-hello',
+            styles: [],
+            template: require("./hello.component.html")
+        })
+        @MyDecorator({
+            templateUrl: 'something'
+        })
+        class MyCmp {}"#
+    );
+
+}
