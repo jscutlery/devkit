df6db034d0e4ec8da0a755021d211c82e45a12d0
Younes Jaaidi <younes@marmicode.io>
2024-03-26 16:02:47 +0100

    refactor: 🛠️ parse inputs
    Co-authored-by: Edouard Bozon <bozonedouard@gmail.com>

--- packages/swc-plugin-angular/src/input_visitor.rs
+++ packages/swc-plugin-angular/src/input_visitor.rs
@@ -1,13 +1,49 @@
-use swc_core::ecma::visit::VisitMut;
+use swc_core::{
+    atoms::Atom,
+    ecma::visit::{VisitMut, VisitMutWith},
+};
 
 #[derive(Default)]
 pub struct InputVisitor {
+    inputs: Vec<InputInfo>,
+    current_component: Atom,
+}
+
+struct InputInfo {
+    component: Atom,
+    name: Atom,
 }
 
 impl VisitMut for InputVisitor {
+    fn visit_mut_class_decl(&mut self, node: &mut swc_core::ecma::ast::ClassDecl) {
+        println!("ClassDecl: {:?}", node.ident.sym);
+        self.current_component = node.ident.sym.clone();
+        node.visit_mut_children_with(self);
+    }
 
+    fn visit_mut_class_prop(&mut self, node: &mut swc_core::ecma::ast::ClassProp) {
+        if let Some(key) = node.key.as_ident() {
+            if let Some(ident) = node
+                .value
+                .as_ref()
+                .and_then(|v| v.as_call())
+                .and_then(|c| c.callee.as_expr())
+                .and_then(|e| e.as_ident())
+            {
+                if ident.sym.eq("input") {
+                    self.inputs.push(InputInfo {
+                        component: self.current_component.clone(),
+                        name: key.sym.clone(),
+                    });
+                }
+            }
+        }
+    }
+
+    fn visit_mut_module(&mut self, module: &mut swc_core::ecma::ast::Module) {
+        module.visit_mut_children_with(self);
+    }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -24,16 +60,17 @@ mod tests {
         r#"
         class MyCmp {
           myInput = input();
+          anotherProperty = 'hello';
         }
         "#,
         r#"
         class MyCmp {
           myInput = input();
+          anotherProperty = 'hello';
         }
         _ts_decorate([
           Input({isSignal: true})
         ], MyCmp.prototype, "myInput");
         "#
     );
-
 }
