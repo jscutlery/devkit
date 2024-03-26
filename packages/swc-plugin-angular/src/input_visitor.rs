83f4b3e9a0526ef58f9305da470141420e2e4bd6
Younes Jaaidi <younes@marmicode.io>
2024-03-26 18:09:22 +0100

    feat(swc-plugin-angular): ✨ transform required signal-inputs

--- packages/swc-plugin-angular/src/input_visitor.rs
+++ packages/swc-plugin-angular/src/input_visitor.rs
@@ -5,6 +5,7 @@ use swc_core::{
         visit::{VisitMut, VisitMutWith},
     },
 };
+use swc_ecma_utils::swc_ecma_ast::Expr;
 use swc_ecma_utils::{ExprExt, ExprFactory};
 
 #[derive(Default)]
@@ -16,6 +17,7 @@ pub struct InputVisitor {
 struct InputInfo {
     component: Atom,
     name: Atom,
+    required: bool,
 }
 
 impl VisitMut for InputVisitor {
@@ -25,21 +27,41 @@ impl VisitMut for InputVisitor {
     }
 
     fn visit_mut_class_prop(&mut self, node: &mut swc_core::ecma::ast::ClassProp) {
-        if let Some(key) = node.key.as_ident() {
-            if let Some(ident) = node
-                .value
-                .as_ref()
-                .and_then(|v| v.as_expr().as_call())
-                .and_then(|c| c.callee.as_expr())
-                .and_then(|e| e.as_ident())
-            {
-                if ident.sym.eq("input") {
+        let key_ident = match node.key.as_ident() {
+            Some(key_ident) => key_ident,
+            None => return,
+        };
+
+        let value_expr = match node
+            .value
+            .as_ref()
+            .and_then(|v| v.as_expr().as_call())
+            .and_then(|c| c.callee.as_expr())
+        {
+            Some(value_expr) => value_expr,
+            None => return,
+        };
+
+        match *value_expr.clone() {
+            Expr::Ident(ident) if ident.sym.eq("input") => {
+                self.inputs.push(InputInfo {
+                    component: self.current_component.clone(),
+                    name: key_ident.sym.clone(),
+                    required: false,
+                });
+            }
+            Expr::Member(member) => {
+                if member.obj.as_ident().map_or(false, |i| i.sym.eq("input"))
+                    && member.prop.ident().map_or(false, |i| i.sym.eq("required"))
+                {
                     self.inputs.push(InputInfo {
                         component: self.current_component.clone(),
-                        name: key.sym.clone(),
+                        name: key_ident.sym.clone(),
+                        required: true,
                     });
                 }
             }
+            _ => return,
         }
     }
 
@@ -49,6 +71,12 @@ impl VisitMut for InputVisitor {
         for input_info in &self.inputs {
             let component = input_info.component.as_str();
             let input_name = input_info.name.as_str();
+            let required_str = if input_info.required {
+                r#",
+        required: true"#
+            } else {
+                ""
+            };
             let raw = Str {
                 span: Default::default(),
                 value: "".into(),
@@ -56,7 +84,7 @@ impl VisitMut for InputVisitor {
                     format!(
                         r#"_ts_decorate([
     require("@angular/core").Input({{
-        isSignal: true
+        isSignal: true{required_str}
     }})
 ], {component}.prototype, "{input_name}")"#
                     )
@@ -91,6 +119,27 @@ mod tests {
           anotherProperty = 'hello';
         }
         _ts_decorate([require("@angular/core").Input({isSignal: true})], MyCmp.prototype, "myInput");
+        "#
+    );
+
+    test_inline!(
+        Syntax::Typescript(TsConfig::default()),
+        |_| as_folder(InputVisitor::default()),
+        decorate_input_required,
+        r#"
+        class MyCmp {
+          myInput = input.required();
+          anotherProperty = 'hello';
+        }
+        "#,
+        r#"
+        class MyCmp {
+          myInput = input.required();
+          anotherProperty = 'hello';
+        }
+        _ts_decorate([
+            require("@angular/core").Input({isSignal: true, required: true})
+        ], MyCmp.prototype, "myInput");
         "#
     );
 }
