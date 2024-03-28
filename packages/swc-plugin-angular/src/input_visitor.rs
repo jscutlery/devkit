77f7f1a2a0a0d9316ba61064f0b679735f61909f
Younes Jaaidi <yjaaidi@gmail.com>
2024-03-28 16:51:24 +0100

    feat(swc-plugin-angular): ✨ transform signal-inputs with alias
    * refactor(swc-plugin-angular): 🛠️parse input alias
    
    * refactor(swc-plugin-angular): 🛠️reorder structs & impls by importance
    
    * test(swc-plugin-angular): ✅ add wide test to inputs with aliases
    
    * feat(swc-plugin-angular): ✨ transform signal-inputs with alias

--- packages/swc-plugin-angular/src/input_visitor.rs
+++ packages/swc-plugin-angular/src/input_visitor.rs
@@ -6,6 +6,9 @@ use swc_core::{
         visit::{VisitMut, VisitMutWith},
     },
 };
+use swc_core::ecma::ast::Lit::Str as LitStr;
+use swc_core::ecma::ast::Prop;
+use swc_core::ecma::visit::{Visit, VisitWith};
 use swc_ecma_utils::{ExprExt, ExprFactory};
 use swc_ecma_utils::swc_ecma_ast::Expr;
 
@@ -13,12 +16,6 @@ use swc_ecma_utils::swc_ecma_ast::Expr;
 pub struct InputVisitor {
     inputs: Vec<InputInfo>,
     current_component: Atom,
-}
-
-struct InputInfo {
-    component: Atom,
-    name: Atom,
-    required: bool,
 }
 
 impl VisitMut for InputVisitor {
@@ -33,62 +30,105 @@ impl VisitMut for InputVisitor {
             None => return,
         };
 
-        let value_expr = match node
+        let call = match node
             .value
-            .as_ref()
+            .as_mut()
             .and_then(|v| v.as_expr().as_call())
-            .and_then(|c| c.callee.as_expr())
+        {
+            Some(call) => call,
+            None => return,
+        };
+
+        let callee = match call.callee.as_expr()
         {
             Some(value_expr) => value_expr,
             None => return,
         };
 
-        match value_expr.as_expr() {
-            Expr::Ident(ident) if ident.sym.eq("input") => {
-                self.inputs.push(InputInfo {
-                    component: self.current_component.clone(),
-                    name: key_ident.sym.clone(),
-                    required: false,
-                });
-            }
+        let required = match callee.as_expr() {
+            /* false if `input()`. */
+            Expr::Ident(ident) if ident.sym.eq("input") => false,
+            /* true if `input.required(). */
             Expr::Member(member) => {
-                if member.obj.as_ident().map_or(false, |i| i.sym.eq("input"))
+                member.obj.as_ident().map_or(false, |i| i.sym.eq("input"))
                     && member.prop.clone().ident().map_or(false, |i| i.sym.eq("required"))
-                {
-                    self.inputs.push(InputInfo {
-                        component: self.current_component.clone(),
-                        name: key_ident.sym.clone(),
-                        required: true,
-                    });
-                }
             }
-            _ => (),
+            _ => return,
+        };
+
+        /* Parse input options. */
+        let mut input_options_visitor = InputOptionsVisitor::default();
+
+        /* Options are either the first or second parameter depending depending on whether
+         * the input is required.
+         * e.g. input.required({alias: '...'}) or input(default, {alias: '...'}) */
+        if let Some(options) = if required { call.args.first() } else { call.args.get(1) } {
+            options.visit_children_with(&mut input_options_visitor);
         }
+
+        self.inputs.push(InputInfo {
+            component: self.current_component.clone(),
+            name: key_ident.sym.clone(),
+            alias: input_options_visitor.alias,
+            required,
+        });
     }
 
     fn visit_mut_module(&mut self, module: &mut swc_core::ecma::ast::Module) {
         module.visit_mut_children_with(self);
 
         for input_info in &self.inputs {
-            let component = input_info.component.as_str();
-            let input_name = input_info.name.as_str();
+            let alias = match &input_info.alias {
+                Some(alias) => format!(r#""{alias}""#),
+                None => "undefined".to_string(),
+            };
+
             let raw = formatdoc! {
                 r#"_ts_decorate([
                     require("@angular/core").Input({{
                         isSignal: true,
+                        alias: {alias},
                         required: {required}
                     }})
-                ], {component}.prototype, "{input_name}")"#,
-                component = component,
-                input_name = input_name,
+                ], {component}.prototype, "{name}")"#,
+                alias = alias,
+                component = input_info.component,
+                name = input_info.name,
                 required = input_info.required
             };
 
             module.body.push(Str {
                 span: Default::default(),
                 value: "".into(),
-                raw: Some(raw.as_str().into())
+                raw: Some(raw.as_str().into()),
             }.into_stmt().into());
+        }
+    }
+}
+
+struct InputInfo {
+    component: Atom,
+    name: Atom,
+    alias: Option<String>,
+    required: bool,
+}
+
+#[derive(Default)]
+struct InputOptionsVisitor {
+    alias: Option<String>,
+}
+
+impl Visit for InputOptionsVisitor {
+    fn visit_prop(&mut self, prop: &Prop) {
+        let key_value = match prop.as_key_value() {
+            Some(key_value) => key_value,
+            None => return,
+        };
+
+        if let Some(true) = key_value.key.as_ident().map(|key| key.sym.eq("alias")) {
+            if let Some(LitStr(str)) = key_value.value.as_lit() {
+                self.alias = Some(str.value.as_str().to_string());
+            }
         }
     }
 }
@@ -115,7 +155,13 @@ mod tests {
           myInput = input();
           anotherProperty = 'hello';
         }
-        _ts_decorate([require("@angular/core").Input({isSignal: true, required: false})], MyCmp.prototype, "myInput");
+        _ts_decorate([
+            require("@angular/core").Input({
+                isSignal: true,
+                alias: undefined,
+                required: false
+            })
+        ], MyCmp.prototype, "myInput");
         "#
     );
 
@@ -135,8 +181,50 @@ mod tests {
           anotherProperty = 'hello';
         }
         _ts_decorate([
-            require("@angular/core").Input({isSignal: true, required: true})
+            require("@angular/core").Input({
+                isSignal: true,
+                alias: undefined,
+                required: true
+            })
         ], MyCmp.prototype, "myInput");
+        "#
+    );
+
+    test_inline!(
+        Syntax::Typescript(TsConfig::default()),
+        |_| as_folder(InputVisitor::default()),
+        decorate_input_alias,
+        r#"
+        class MyCmp {
+          aliasedInput = input(undefined, {alias: 'myInputAlias'});
+          nonAliasedInput = input({
+            alias: 'this_is_a_default_value_not_an_alias'
+          });
+          anotherProperty = 'hello';
+        }
+        "#,
+        r#"
+        class MyCmp {
+          aliasedInput = input(undefined, {alias: 'myInputAlias'});
+          nonAliasedInput = input({
+            alias: 'this_is_a_default_value_not_an_alias'
+          });
+          anotherProperty = 'hello';
+        }
+        _ts_decorate([
+            require("@angular/core").Input({
+                isSignal: true,
+                alias: "myInputAlias",
+                required: false
+            })
+        ], MyCmp.prototype, "aliasedInput");
+        _ts_decorate([
+            require("@angular/core").Input({
+                isSignal: true,
+                alias: undefined,
+                required: false
+            })
+        ], MyCmp.prototype, "nonAliasedInput");
         "#
     );
 }
