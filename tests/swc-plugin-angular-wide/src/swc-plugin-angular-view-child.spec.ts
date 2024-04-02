22b873424e5af531410d43ff0113403e222d915f
Younes Jaaidi <younes@marmicode.io>
2024-04-02 17:59:40 +0200

    test(swc-plugin-angular): ✅ add tests

--- tests/swc-plugin-angular-wide/src/swc-plugin-angular-view-child.spec.ts
+++ tests/swc-plugin-angular-wide/src/swc-plugin-angular-view-child.spec.ts
@@ -4,7 +4,7 @@ import { Component, ElementRef, Type, viewChild } from '@angular/core';
 import { createComponent } from './testing';
 
 describe('swc-plugin-angular: view child', () => {
-  it('should match view child', () => {
+  it('should match view child without read option', () => {
 
     @Component({
       standalone: true,
@@ -14,7 +14,7 @@ describe('swc-plugin-angular: view child', () => {
       `
     })
     class Container {
-      titleEl = viewChild('title', { read: ElementRef });
+      titleEl = viewChild<ElementRef<HTMLHeadingElement>>('title');
       paragraph = () => this.titleEl()?.nativeElement.textContent;
     }
 
@@ -22,7 +22,77 @@ describe('swc-plugin-angular: view child', () => {
     expect(paragraph).toBe('Hello view child!');
   });
 
-  it.todo('should match required view child');
+  it('should match view child with read option', () => {
+
+    @Component({
+      standalone: true,
+      selector: 'jsc-title',
+      template: `<h1>{{ title }}</h1>`
+    })
+    class Title {
+      title = 'Hello view child with read option!';
+    }
+
+    @Component({
+      standalone: true,
+      imports: [Title],
+      template: `
+        <jsc-title #title />
+        <p>{{ paragraph() }}</p>
+      `
+    })
+    class Container {
+      titleEl = viewChild('title', { read: Title });
+      paragraph = () => this.titleEl()?.title;
+    }
+
+    const { paragraph } = render(Container);
+    expect(paragraph).toBe('Hello view child with read option!');
+  });
+
+  it('should match required view child', () => {
+    @Component({
+      standalone: true,
+      template: `
+        <h1 #title>Hello required view child!</h1>
+        <p>{{ paragraph() }}</p>
+      `
+    })
+    class Container {
+      titleEl = viewChild.required('title', { read: ElementRef<HTMLHeadingElement> });
+      paragraph = () => this.titleEl().nativeElement.textContent;
+    }
+
+    const { paragraph } = render(Container);
+    expect(paragraph).toBe('Hello required view child!');
+  });
+
+  it('should match view child with provider', () => {
+    @Component({
+      standalone: true,
+      selector: 'jsc-title',
+      template: `<h1>{{ title }}</h1>`
+    })
+    class Title {
+      title = 'Hello view child with provider!';
+    }
+
+    @Component({
+      standalone: true,
+      imports: [Title],
+      template: `
+        <jsc-title #title />
+        <p>{{ paragraph() }}</p>
+      `
+    })
+    class Container {
+      titleEl = viewChild(Title);
+      paragraph = () => this.titleEl()?.title;
+    }
+
+    const { paragraph } = render(Container);
+    expect(paragraph).toBe('Hello view child with provider!');
+  });
 
   it.todo('should match view children');
 
