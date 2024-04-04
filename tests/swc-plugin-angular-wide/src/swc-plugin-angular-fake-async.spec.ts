87c49edd133b92351f431e6a3f72ec3a621765d4
Edouard Bozon <bozonedouard@gmail.com>
2024-04-04 09:46:50 +0200

    test(swc-plugin-angular): ✅️ add fakeAsync + tick spec

---
+++ tests/swc-plugin-angular-wide/src/swc-plugin-angular-fake-async.spec.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { fakeAsync, tick } from '@angular/core/testing';
+import { createComponent } from './testing';
+
+describe('swc-plugin-angular: fakeAsync and tick', () => {
+  it('should render with fakeAsync and tick', fakeAsync(() => {
+    @Component({
+      standalone: true,
+      template: '<h1>Value: {{ value() }}</h1>',
+    })
+    class Container implements OnInit {
+      value = signal(0);
+
+      ngOnInit() {
+        setTimeout(() => {
+          this.value.set(1);
+        }, 1000);
+      }
+    }
+
+    const { nativeElement } = createComponent(Container);
+    const heading = () => nativeElement.querySelector('h1')?.textContent;
+
+    expect(heading()).toBe('Value: 0');
+
+    tick(1000);
+    expect(heading()).toBe('Value: 1');
+  }));
+});
