87994509cb0be6c06cecc4ecb9fbaf4981187e29
Younes Jaaidi <younes@marmicode.io>
2024-04-04 18:21:07 +0200

    test(swc-plugin-angular): ✅ set up vitest
    #305

---
+++ tests/swc-plugin-angular-wide/vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      cache: {
+        dir: '../../node_modules/.vitest'
+      },
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      setupFiles: ['src/test-setup-vitest.ts'],
+      reporters: ['default'],
+      coverage: {
+        reportsDirectory: '../../coverage/apps/demo',
+        provider: 'v8'
+      }
+    }
+  })
+);
