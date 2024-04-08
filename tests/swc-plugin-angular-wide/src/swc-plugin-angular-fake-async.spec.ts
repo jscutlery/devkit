74bba958167f275556fe30ec40951d336d0fe3d1
Younes Jaaidi <younes@marmicode.io>
2024-04-09 00:27:00 +0200

    fix(swc-plugin-angular): 🐞 transform async/await (#313)
    We have to transform async/await to generators,
    otherwise Zone.js will not trigger tick, so no CD when an awaited promise is resolved.
    This also allows using `fakeAsync + tick`.
    
    Closes #303

--- tests/swc-plugin-angular-wide/src/swc-plugin-angular-fake-async.spec.ts
+++ tests/swc-plugin-angular-wide/src/swc-plugin-angular-fake-async.spec.ts
@@ -1,31 +1,12 @@
 /* eslint-disable @angular-eslint/component-class-suffix */
 
-import { Component, OnInit, signal } from '@angular/core';
 import { fakeAsync, tick } from '@angular/core/testing';
-import { createComponent } from './testing';
 
-describe('swc-plugin-angular: fakeAsync and tick', () => {
-  it('should render with fakeAsync and tick', fakeAsync(() => {
-    @Component({
-      standalone: true,
-      template: '<h1>Value: {{ value() }}</h1>',
-    })
-    class Container implements OnInit {
-      value = signal(0);
+describe('swc-plugin-angular: fakeAsync', () => {
+  /* Cf. https://github.com/jscutlery/devkit/issues/303 */
+  it('should work with fakeAsync + tick + async/await', fakeAsync(async () => {
+    await Promise.resolve();
 
-      ngOnInit() {
-        setTimeout(() => {
-          this.value.set(1);
-        }, 1000);
-      }
-    }
-
-    const { nativeElement } = createComponent(Container);
-    const heading = () => nativeElement.querySelector('h1')?.textContent;
-
-    expect(heading()).toBe('Value: 0');
-
-    tick(1000);
-    expect(heading()).toBe('Value: 1');
+    expect(() => tick()).not.toThrow();
   }));
 });
