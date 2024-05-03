c32f1d292e5063e76770fba87a2fcdeeaa7a3a76
Younes Jaaidi <younes@marmicode.io>
2024-05-03 21:49:53 +0200

    test(playwright-ct-angular): ✅ add test for output unmount

--- tests/playwright-ct-angular-wide/tests/output.pw.ts
+++ tests/playwright-ct-angular-wide/tests/output.pw.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@jscutlery/playwright-ct-angular';
+import { OutputUnsubscriptionComponent } from '../src/output-unsubscription.component';
 import { OutputComponent } from '../src/output.component';
 
 test('output', async ({ mount }) => {
@@ -41,4 +42,24 @@ test.skip('replace existing listener when new listener is set', async ({
   await component.click();
   expect(firstListenerCalled).toBe(false);
   expect(secondListenerCalled).toBe(true);
+});
+
+test('unsubscribe from output when the component is unmounted', async ({
+  mount,
+  page,
+}) => {
+  const component = await mount(OutputUnsubscriptionComponent, {
+    on: {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      selectMagicValue() {},
+    },
+  });
+
+  await component.unmount();
+
+  /* Check that the output observable had been unsubscribed from
+   * as it sets a global variable `hasUnusbscribed` to true
+   * when it detects unsubscription. Cf. OutputComponent. */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  expect(await page.evaluate(() => (window as any).hasUnsubscribed)).toBe(true);
 });
