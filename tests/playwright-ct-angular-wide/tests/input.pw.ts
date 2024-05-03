182f8f39fba0ce63428348d2618f66b92cfb343d
Younes Jaaidi <younes@marmicode.io>
2024-05-04 00:23:52 +0200

    test(playwright-ct-angular): ✅ add test for input update

--- tests/playwright-ct-angular-wide/tests/input.pw.ts
+++ tests/playwright-ct-angular-wide/tests/input.pw.ts
@@ -10,3 +10,19 @@ test('input', async ({ mount }) => {
 
   await expect(component).toContainText('Hello world!');
 });
+
+test.skip('update input', async ({ mount }) => {
+  const component = await mount(InputComponent, {
+    props: {
+      title: 'Hello world!',
+    },
+  });
+
+  await component.update({
+    props: {
+      title: 'Hello france!',
+    },
+  });
+
+  await expect(component).toContainText('Hello france!');
+});
