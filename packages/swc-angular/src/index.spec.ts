99c57254c539f009b457df34cfbceb931647cfdd
Younes Jaaidi <younes@marmicode.io>
2024-05-03 10:38:49 +0200

    fix(swc-angular): 🐞 fix stackblitz support (#364)
    fallback to checking version from package.json when package doesn't export version

--- packages/swc-angular/src/index.spec.ts
+++ packages/swc-angular/src/index.spec.ts
@@ -1,4 +1,6 @@
 import { afterEach, expect, test, vi } from 'vitest';
+import { FileSystem } from './utils';
+import { join } from 'node:path';
 
 /**
  * Using an old version of `@swc/core` will simply crash with the following error:
@@ -48,13 +50,69 @@ test('should not throw an error when module is imported and version is ~1.4.0', 
   expect(process.exit).not.toHaveBeenCalledOnce();
 });
 
+test('should fallback to package.json if version is not available (this happens on stackblitz)', async () => {
+  const { fileSystem } = setUp();
+
+  vi.doMock('@swc/core', () => ({
+    version: undefined,
+  }));
+  fileSystem.setJsonFile('node_modules/@swc/core/package.json', {
+    version: '1.4.0',
+  });
+
+  await import('./index');
+
+  expect(console.error).not.toHaveBeenCalledOnce();
+  expect(process.exit).not.toHaveBeenCalledOnce();
+});
+
+test('should throw an error if version from package.json is not compatible', async () => {
+  const { fileSystem } = setUp();
+
+  vi.doMock('@swc/core', () => ({
+    version: undefined,
+  }));
+  fileSystem.setJsonFile('node_modules/@swc/core/package.json', {
+    version: '1.3.0',
+  });
+
+  await import('./index');
+
+  expect(console.error).toHaveBeenCalledWith(
+    expect.stringContaining(
+      `@swc/core version 1.3.0 is incompatible with @jscutlery/swc-angular`,
+    ),
+  );
+});
+
 function setUp() {
+  const fileSystem = new FileSystemFake();
   vi.resetModules();
+  vi.doMock('./utils', () => ({
+    fileSystem,
+  }));
   vi.spyOn(console, 'error').mockReturnValue();
   vi.spyOn(process, 'exit').mockReturnValue(undefined as never);
+  return { fileSystem };
 }
 
 afterEach(() => {
   vi.restoreAllMocks();
   vi.resetModules();
 });
+
+class FileSystemFake implements FileSystem {
+  #jsonFiles = new Map<string, any>();
+  #workspaceRoot = join(__dirname, '../../..');
+
+  readJsonFile(path: string) {
+    return this.#jsonFiles.get(path);
+  }
+
+  /**
+   * Set the content of a JSON file using the workspace root as the base path.
+   */
+  setJsonFile(path: string, content: any) {
+    this.#jsonFiles.set(join(this.#workspaceRoot, path), content);
+  }
+}
