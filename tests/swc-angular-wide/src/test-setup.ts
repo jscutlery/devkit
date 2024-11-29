c365aa267acd49c9ef75ed2037b14244034f4d8a
Dmitry Zakharov <pumano@gmail.com>
2024-11-29 17:48:36 +0300

    chore: update nx monorepo (#678)
    closes #612

--- tests/swc-angular-wide/src/test-setup.ts
+++ tests/swc-angular-wide/src/test-setup.ts
@@ -5,8 +5,9 @@ globalThis.ngJest = {
     errorOnUnknownProperties: true,
   },
 };
-import 'jest-preset-angular/setup-jest';
 import 'reflect-metadata';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 import { it } from '@jest/globals';
 
 (globalThis as any).it.fails = it.failing;
+setupZoneTestEnv();
--- tests/swc-angular-wide/vitest.config.mts
+++ tests/swc-angular-wide/vitest.config.mts
@@ -5,6 +5,7 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      watch: false,
       globals: true,
       environment: 'jsdom',
       include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
