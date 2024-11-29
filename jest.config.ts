5ed5dfad47684a0654b667648648de0dae573c7a
Dmitry Zakharov <pumano@gmail.com>
2024-11-29 15:56:59 +0300

    chore: update nx monorepo
    closes #612

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,5 @@
-const { getJestProjects } = require('@nx/jest');
+import { getJestProjectsAsync } from '@nx/jest';
 
-module.exports = { projects: getJestProjects() };
+export default async () => ({
+  projects: await getJestProjectsAsync(),
+});
